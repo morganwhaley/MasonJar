05ed3fb37069bf942f5e13e1d0b8ab14adfd87cf
Morgan Whaley <morganwhaley@integer.com>
2014-10-21 09:27:30 -0600

    “Restore from before reset”

--- scripts/masonjar.1.0.0.js
+++ scripts/masonjar.1.0.0.js
@@ -1,7 +1,7 @@
 /*
-* Mason Jar - The non-lamesauce social media feed
-* v 1.0.0
-*/
+ * Mason Jar - The non-lamesauce social media feed
+ * v 1.0.0
+ */
 
 
 var MasonJar = function() { };
@@ -60,19 +60,23 @@ MasonJar.prototype.init = function() {
         }
     };
 
-    this.options = {
-        overlaycolor: '#26d798',
-        columns: 4
+    this.defaultSettings = {
+        maxColumns: 6,
+        rows: 3
     };
+
 };
 
 
 MasonJar.prototype.get = function(request) {
-    //console.log('request' + request.name);
 
     if(!this.setRequestByRule(request)) {
         return false;
         // Possibly send back an error or graceful failure
+    }
+
+    if (request.settings) {
+        this.extendDefaults(request.settings);
     }
 
     if (this.request.method) {
@@ -94,15 +98,19 @@ MasonJar.prototype.setRequestByRule = function(request) {
 };
 
 
-MasonJar.prototype.create = function() {
 
+MasonJar.prototype.extendDefaults = function(userSettings) {
+    if (this.defaultSettings[userSettings]) {
+        this.request = $.extend(true, userSettings, this.defaultSettings);
+    }
 };
+
 
 
 MasonJar.prototype.setupFacebookFeed = function() {
     /* make the API call */
     FB.api(
-        '/' +  + '/posts',
+            '/' +  + '/posts',
         function (response) {
             if (response && !response.error) {
                 console.log(response);
@@ -140,11 +148,11 @@ MasonJar.prototype.normalizeInstagramResult = function() {};
 
 
 /*
-* ===========================================================================
-* BEGIN CANNERY CLASS
-* Renders normalized social media objects
-* ===========================================================================
-*/
+ * ===========================================================================
+ * BEGIN CANNERY CLASS
+ * Renders normalized social media objects
+ * ===========================================================================
+ */
 
 
 Cannery.prototype.init = function(options) {
@@ -153,21 +161,61 @@ Cannery.prototype.init = function(options) {
         this.requester = new MasonJar();
         this.requester.init();
         this.socialItems = [];
+        this.optionsSettings = [];
         options.feeds.forEach(function(request) {
             context.socialItems.push(context.requester.get(request));
         });
+        options.settings.forEach(function(request) {
+            context.optionsSettings.push(context.requester.get(request));
+        });
     }
-    //console.log('options: ' + options);
     this.createFakeImageResult();
     //this.createFakeTextResult();
     this.bindEvents();
+    this.setupGridSizing();
     this.buildIsotopeGrid();
 };
 
 
 Cannery.prototype.bindEvents = function() {
     var context = this;
-    //$('.jar').hover(function(e) { context.setVisibility(e); });
+    $(window).resize(function() { context.setupGridSizing(); });
+    $('.jar').hover(function(e) { context.setVisibility(e); });
+};
+
+
+Cannery.prototype.setupGridSizing = function() {
+    var maxCols = 5;
+    var windowWidth = $(window).width();
+    var columnCount = this.checkColumnLayout(maxCols, windowWidth);
+    var jarSize = windowWidth / columnCount;
+    var elements = ['.jar', '.holder', '.jar > img'];
+    this.setHeightsAndWidths(jarSize, elements);
+};
+
+
+Cannery.prototype.checkColumnLayout = function(columns, windowWidth) {
+    for (var i = columns; i > 0; i--) {
+        if (windowWidth / i > 280) {
+            columns = i;
+            break;
+        }
+    }
+    return columns;
+};
+
+
+Cannery.prototype.setHeightsAndWidths = function(size, elements) {
+    var newsize;
+    for (var i = 0; i < elements.length; i++ ) {
+        if (elements[i] == '.holder') {
+            newsize = size - 60;
+        }
+        else {
+            newsize = size;
+        }
+        $(elements[i]).height(newsize).width(newsize);
+    }
 };
 
 
@@ -190,7 +238,9 @@ Cannery.prototype.createFakeTextResult = function() {
         link: 'http://twitter.com/supwhaley',
         datedatapoint: 'datetime'
     };
-    this.buildContainer(this.resultItem);
+    for (var i = 0; i < 15; i++) {
+        this.buildContainer(this.resultItem);
+    }
 };
 
 
@@ -205,7 +255,7 @@ Cannery.prototype.createFakeImageResult = function() {
         link: 'http://instagram.com/seahorse5280',
         datedatapoint: 'datetime'
     };
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 15; i++) {
         this.buildContainer(this.resultItem);
     }
 };
@@ -230,26 +280,20 @@ Cannery.prototype.buildContainer = function(item) {
         '<data class="' + item.datedatapoint + ' posted-date" title="1409232647" value="">' + item.time + '</data>' +
         '<div class="social-icon"><i class="fa '+ item.icon + '"></i></div>' +
         '</div>' +
-        image +
         '<div class="overlay"></div>' +
+        image +
         '</div>';
     $('#masonJar').append(listItem);
 };
 
 
 Cannery.prototype.setVisibility = function(event) {
-    var $target = $(event.target) != $('div.holder') ? $(event.target).find('div.holder') : $(event.target);
-    console.log($target);
-    if ($target.hasClass('has-image')) {
-        if ($target.hasClass('show')) {
-            $target.removeClass('show');
-            $target.siblings('.overlay').removeClass('show');
-        }
-        else {
-            $target.addClass('show');
-            $target.siblings('.overlay').addClass('show');
-        }
+    var $target = $(event.target);
+    if ($target.hasClass('.holder') == false) {
+        $target = $target.closest('.holder');
     }
+    $target.toggleClass('show');
+    $target.next('.overlay').toggleClass('show');
 };
 
 
